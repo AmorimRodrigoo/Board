7a994fea71707e70f7ca57c1ad2a2d084c94f704
Rodrigo Amorim <amorimrodrigo071@gmail.com>
2025-03-01 15:34:51 -0300

    FindByBoardId/BoardColumnDAO

--- src/main/java/com/rodrigo/Board/percistence/dao/BoardColumnDAO.java
+++ src/main/java/com/rodrigo/Board/percistence/dao/BoardColumnDAO.java
@@ -6,7 +6,10 @@ import lombok.AllArgsConstructor;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+
+import static com.rodrigo.Board.percistence.entity.BoardColumnKindEnum.findByName;
 
 @AllArgsConstructor
 public class BoardColumnDAO {
@@ -29,7 +32,22 @@ public class BoardColumnDAO {
         }
     }
 
-    public List<BoardColumnEntity> findById(Long id) throws SQLException{
+    public List<BoardColumnEntity> findByBoardId(Long id) throws SQLException{
+        var sql = "SELECT id, name, `order´ FROM BOARDS_COLUMNS WHERE board_id = ? ORDER BY ´order´";
+        List<BoardColumnEntity> entities = new ArrayList<>();
+        try (var statement = connection.prepareStatement(sql)){
+            statement.setLong(1, id);
+            statement.executeQuery();
+            var resultSet = statement.getResultSet();
+            while (resultSet.next()){
+                var entity = new BoardColumnEntity();
+                entity.setId(resultSet.getLong("id"));
+                entity.setName(resultSet.getString("name"));
+                entity.setOrder(resultSet.getInt("order"));
+                entity.setKind(findByName(resultSet.getString("kind")));
+                entities.add(entity);
+            }
+        }
         return null;
     }
 }
