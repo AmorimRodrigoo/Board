926bb07467a9c58d095882f877ba57d3143d9ff6
Rodrigo Amorim <amorimrodrigo071@gmail.com>
2025-03-03 22:58:51 -0300

    CardDetailsDTO

---
+++ src/main/java/com/rodrigo/Board/dto/CardDetailsDTO.java
@@ -0,0 +1,16 @@
+package com.rodrigo.Board.dto;
+
+import java.time.OffsetDateTime;
+
+public record CardDetailsDTO(
+        Long id,
+        String title,
+        String description,
+        boolean blocked,
+        OffsetDateTime blockedAt,
+        String blockReason,
+        int blockAmount,
+        Long columnId,
+        String columnName
+) {
+}
