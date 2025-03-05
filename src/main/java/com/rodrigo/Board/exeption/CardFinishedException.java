f1747535769754cd23eae7a23c88c3771ceb0410
Rodrigo Amorim <amorimrodrigo071@gmail.com>
2025-03-05 17:40:26 -0300

    CardFinishedException

---
+++ src/main/java/com/rodrigo/Board/exeption/CardFinishedException.java
@@ -0,0 +1,8 @@
+package com.rodrigo.Board.exeption;
+
+public class CardFinishedException extends RuntimeException {
+
+    public CardFinishedException(String message) {
+        super(message);
+    }
+}
