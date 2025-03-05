5bae3d3670dd436dbf7051775355c094f2937fb1
Rodrigo Amorim <amorimrodrigo071@gmail.com>
2025-03-05 17:43:32 -0300

    moveToNextColumn  method

--- src/main/java/com/rodrigo/Board/services/CardService.java
+++ src/main/java/com/rodrigo/Board/services/CardService.java
@@ -1,12 +1,20 @@
 package com.rodrigo.Board.services;
 
 
+import com.rodrigo.Board.dto.BoardColumnInfoDTO;
+import com.rodrigo.Board.exeption.CardBlockedException;
+import com.rodrigo.Board.exeption.CardFinishedException;
+import com.rodrigo.Board.exeption.EntityNotFoundException;
 import com.rodrigo.Board.percistence.dao.CardDAO;
 import com.rodrigo.Board.percistence.entity.CardEntity;
 import lombok.AllArgsConstructor;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
+
+import static com.rodrigo.Board.percistence.entity.BoardColumnKindEnum.FINAL;
+
 
 @AllArgsConstructor
 public class CardService {
@@ -21,6 +29,36 @@ public class CardService {
             return entity;
         }catch (SQLException ex){
             connection.rollback();
+            throw ex;
+        }
+    }
+
+    public void moveToNextColumn(
+            final Long cardId, final List<BoardColumnInfoDTO> boardColumnsInfo) throws SQLException {
+        try{
+            var dao = new CardDAO(connection);
+            var optional = dao.findByID(cardId);
+            var dto = optional.orElseThrow(
+                    () -> new EntityNotFoundException("Card de id %s não foi encontrado".formatted(cardId))
+            );
+            if (dto.blocked()){
+                var message = "O card %s está bloqueado, desbloqueie para mover".formatted(cardId);
+                throw new CardBlockedException(message);
+            }
+            var currentColumn = boardColumnsInfo.stream()
+                    .filter(bc -> bc.id().equals(dto.columnId()))
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalStateException("O card informado pertence a outro board"));
+            if (currentColumn.kind().equals(FINAL)){
+                throw new CardFinishedException("O card já está finalizado");
+            }
+            var nextColumn = boardColumnsInfo.stream()
+                    .filter(bc -> bc.order() == currentColumn.order() + 1)
+                    .findFirst().orElseThrow();
+            dao.moveToColumn(nextColumn.id(), cardId);
+            connection.commit();
+        }catch (SQLException ex){
+                connection.rollback();
             throw ex;
         }
     }
