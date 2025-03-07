5b692ca77dd0fa0a8bbe4e2d52d122e30a2245ec
Rodrigo Amorim <amorimrodrigo071@gmail.com>
2025-03-07 18:33:41 -0300

    cancel method

--- src/main/java/com/rodrigo/Board/services/CardService.java
+++ src/main/java/com/rodrigo/Board/services/CardService.java
@@ -2,6 +2,7 @@ package com.rodrigo.Board.services;
 
 
 import com.rodrigo.Board.dto.BoardColumnInfoDTO;
+import com.rodrigo.Board.dto.CardDetailsDTO;
 import com.rodrigo.Board.exeption.CardBlockedException;
 import com.rodrigo.Board.exeption.CardFinishedException;
 import com.rodrigo.Board.exeption.EntityNotFoundException;
@@ -27,7 +28,7 @@ public class CardService {
             dao.insert(entity);
             connection.commit();
             return entity;
-        }catch (SQLException ex){
+        } catch (SQLException ex) {
             connection.rollback();
             throw ex;
         }
@@ -35,13 +36,13 @@ public class CardService {
 
     public void moveToNextColumn(
             final Long cardId, final List<BoardColumnInfoDTO> boardColumnsInfo) throws SQLException {
-        try{
+        try {
             var dao = new CardDAO(connection);
             var optional = dao.findByID(cardId);
             var dto = optional.orElseThrow(
                     () -> new EntityNotFoundException("Card de id %s não foi encontrado".formatted(cardId))
             );
-            if (dto.blocked()){
+            if (dto.blocked()) {
                 var message = "O card %s está bloqueado, desbloqueie para mover".formatted(cardId);
                 throw new CardBlockedException(message);
             }
@@ -49,17 +50,47 @@ public class CardService {
                     .filter(bc -> bc.id().equals(dto.columnId()))
                     .findFirst()
                     .orElseThrow(() -> new IllegalStateException("O card informado pertence a outro board"));
-            if (currentColumn.kind().equals(FINAL)){
+            if (currentColumn.kind().equals(FINAL)) {
                 throw new CardFinishedException("O card já está finalizado");
             }
             var nextColumn = boardColumnsInfo.stream()
                     .filter(bc -> bc.order() == currentColumn.order() + 1)
-                    .findFirst().orElseThrow();
+                    .findFirst().orElseThrow(() -> new IllegalStateException("O card está cancelado"));
             dao.moveToColumn(nextColumn.id(), cardId);
             connection.commit();
-        }catch (SQLException ex){
-                connection.rollback();
+        } catch (SQLException ex) {
+            connection.rollback();
             throw ex;
         }
     }
+
+    public void cancel(final Long cardId, final Long cancelColumnId, final List<BoardColumnInfoDTO> boardColumnsInfo) throws SQLException {
+        try {
+            var dao = new CardDAO(connection);
+            var optional = dao.findByID(cardId);
+            var dto = optional.orElseThrow(
+                    () -> new EntityNotFoundException("Card de id %s não foi encontrado".formatted(cardId))
+            );
+            if (dto.blocked()) {
+                var message = "O card %s está bloqueado, desbloqueie para mover".formatted(cardId);
+                throw new CardBlockedException(message);
+            }
+            var currentColumn = boardColumnsInfo.stream()
+                    .filter(bc -> bc.id().equals(dto.columnId()))
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalStateException("O card informado pertence a outro board"));
+            if (currentColumn.kind().equals(FINAL)) {
+                throw new CardFinishedException("O card já está finalizado");
+            }
+            boardColumnsInfo.stream()
+                    .filter(bc -> bc.order() == currentColumn.order() + 1)
+                    .findFirst().orElseThrow(() -> new IllegalStateException("O card está cancelado"));
+            dao.moveToColumn(cancelColumnId, cardId);
+            connection.commit();
+        } catch (SQLException ex) {
+            connection.rollback();
+            throw ex;
+        }
+    }
+
 }
